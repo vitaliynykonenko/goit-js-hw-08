5d3e5128a504fd788e8d708e6fcbd9486b209a49
Vitaliy Nykonenko <vnykonenko@ukr.net>
2022-08-18 19:48:01 +0300

    Перша частина ДЗ

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,33 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+// import "../css/01-gallery.css";
+// import "../css/common.css";
+
 console.log(galleryItems);
+
+const imgContainer = document.querySelector('.gallery');
+const imgGallery = createImagesMarkup(galleryItems);
+imgContainer.insertAdjacentHTML('beforeend', imgGallery);
+
+function createImagesMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+    <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+
+    </div>
+`;
+    })
+    .join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
